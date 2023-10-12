cf018e5fb985e8b6fb391a1105cfa8bfcdb0da76
Matorry <rodri.martinceron@gmail.com>
2023-10-12 11:03:55 +0200

    Add header and footer component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,23 +3,25 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 
-describe('AppComponent', () => {
-  beforeEach(() =>
-    TestBed.configureTestingModule({
-      imports: [RouterTestingModule, CoreModule],
-      declarations: [AppComponent],
-    })
-  );
+describe('Given the component AppComponent', () => {
+  describe('When i instance it ', () => {
+    beforeEach(() =>
+      TestBed.configureTestingModule({
+        imports: [RouterTestingModule, CoreModule],
+        declarations: [AppComponent],
+      })
+    );
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    it('Then, the component should be created.', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      expect(app).toBeTruthy();
+    });
 
-  it(`should have as title 'e-commerce'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('e-commerce');
+    it(`Then, should have as title 'e-commerce'`, () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      expect(app.title).toEqual('e-commerce');
+    });
   });
 });
