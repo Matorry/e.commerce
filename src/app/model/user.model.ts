00b6847fe70bb675e3b237bb2c8f478afcc088ec
Matorry <rodri.martinceron@gmail.com>
2023-10-14 12:25:19 +0200

    Add user repository with tests and user data model

---
+++ src/app/model/user.model.ts
@@ -0,0 +1,29 @@
+export type WithId = {
+  id: string;
+};
+
+export type LoginData = {
+  userName: string;
+  password: string;
+};
+
+export type UserNoId = LoginData & {
+  firstName: string;
+  lastName: string;
+  email: string;
+  age: string;
+  phone: string;
+  addressStreet: string;
+  postalCode: string;
+  city: string;
+  title: string;
+  dateOfBirth: string;
+  isAcceptingCommunications: boolean;
+};
+
+export type User = WithId & UserNoId;
+
+export type LogedUser = {
+  user: User;
+  token: string;
+};
